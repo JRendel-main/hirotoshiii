4854602175dc47e47ca5f0f3ba8113108db86629
JRendel-main <112527772+JRendel-main@users.noreply.github.com>
2022-09-23 11:54:46 +0800

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
+	</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<label>Home</label>
+		</div>
+	</nav>
+	<div class="col-md-3"></div>
+	<div class="col-md-6 well">
+		<h3 class="text-primary">NEUST REGISTERING SYSTEM</h3>
+		<hr style="border-top:1px dotted #ccc;" />
+		<div class="col-md-3"></div>
+		<div class="col-md-6">
+			<form method="POST" action="add.php">
+				<div class="form-group">
+					<label>Firstname</label>
+					<input class="form-control" type="text" name="firstname" required/>
+				</div>
+				<div class="form-group">
+					<label>Lastname</label>
+					<input class="form-control" type="text" name="lastname" required/>
+				</div>
+				<div class="form-group">
+					<label>Paid Status</label> 
+					<input class="form-control" type="text" name="paidtatus" required/>
+				</div>
+				<div class="form-group">
+					<label>Year/Section</label> 
+					<input class="form-control" type="text" name="yearsection" required/>
+				</div>
+				<div class="form-group">
+					<label>Claim Status</label> 
+					<input class="form-control" type="text" name="claimstatus" required/>
+				</div>
+				<div class="form-group">
+					<button class="btn btn-primary form-control" type="submit" name="save">Save</button>
+				</div>
+			</form>
+		</div>
+		<table class="table table-bordered">
+			<thead class="alert-danger">
+				<tr>
+					<th>Receipt #</th>
+					<th>Firstname</th>
+					<th>Lastname</th>
+					<th>Paid Status</th>
+					<th>Year/Section</th>
+					<th>Claim Status</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody class="alert-warning">
+				<?php
+					require 'connection.php';
+					$sql = $conn->prepare("SELECT * FROM `member` ORDER BY `mem_id` DESC");
+					$sql->execute();
+					while($row = $sql->fetch()){
+				?>
+				<tr>
+					<td><?php echo $row['mem_id']?></td>
+					<td><?php echo $row['firstname']?></td>
+					<td><?php echo $row['lastname']?></td>
+					<td><?php echo $row['paidstatus']?></td>
+					<td><?php echo $row['yearsection']?></td>
+					<td><?php echo $row['claimstatus']?></td>
+					<td><?php echo $row['address']?></td>
+					
+					<td><a href="edit.php?id=<?php echo $row['mem_id']?>">Edit</a> | <a href="delete.php?id=<?php echo $row['mem_id']?>">Delete</a></td>
+				</tr>
+				<?php
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
+	
+</body>
+	
+</html>
